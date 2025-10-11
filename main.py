5908e2e32ee9192452d3007c392929cd8a0b3534
Rahul Dandotiya <rahuldandotiya@Rahuls-MacBook-Air.local>
2025-10-11 13:06:15 +0530

    Initial commit - added project

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on Render!"
+    return render_template("app.html") 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
